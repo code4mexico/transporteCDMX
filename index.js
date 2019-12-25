580f04462d5ea2a6d6ba8a32a0b5b5b146e05a1c
kernel_init <c4m@tello.io>
2019-12-24 18:36:45 -0600

    react-navigation dependencies

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import 'react-native-gesture-handler'
+import { AppRegistry, YellowBox } from 'react-native';
+import App from './src/App';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+YellowBox.ignoreWarnings(['Remote debugger'])
