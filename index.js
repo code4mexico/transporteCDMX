3b077090ff5dd6af9b8d2124ae250d462817e616
kernel_init <c4m@tello.io>
2019-12-24 18:47:00 -0600

    prettier

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler'
-import { AppRegistry, YellowBox } from 'react-native';
-import App from './src/App';
-import { name as appName } from './app.json';
+import { AppRegistry, YellowBox } from 'react-native'
+import App from './src/App'
+import { name as appName } from './app.json'
 
 AppRegistry.registerComponent(appName, () => App)
 
