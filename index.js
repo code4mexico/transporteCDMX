6075c2d77b2db12e5fa3a49751f46fc11ed33fd0
kernel_init <c4m@tello.io>
2019-12-24 18:42:45 -0600

    prettier

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import { AppRegistry, YellowBox } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App)
 
 YellowBox.ignoreWarnings(['Remote debugger'])
